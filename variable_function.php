a7675b090fc08be635cb97504625a72b4e5f18c2
ubuntu <datagit@yahoo.com>
2015-09-06 15:59:53 +0700

    variable function

---
+++ variable_function.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ubuntu
+ * Date: 06/09/2015
+ * Time: 15:44
+ */
+
+function foo() {
+    echo "run func foo()\n";
+}
+
+function bar($arg) {
+    echo "run func bar() with param [{$arg}]\n";
+}
+
+//$foo = 'foo';
+//$foo();
+//
+//$bar = 'bar';
+//$bar(100);
+
+class Foo {
+    private $name = "Dat.Dao";
+    public function printName($m, $n = 1) {
+        var_dump($m, $n);
+        for($i = 0; $i < $n; $i++) {
+            echo "my name is {$this->name}\n";
+        }
+
+    }
+}
+
+$f1 = new Foo;
+$fun = 'printName';
+//$f1->$fun(2);
+
+$obj = 'Foo';
+//$fun2 = array(new $obj, 'printName');
+//$fun2();
+
+call_user_func(array(new $obj, 'printName'), 2, 3);
